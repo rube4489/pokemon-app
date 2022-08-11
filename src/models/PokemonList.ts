0006989f5ebbcdad8a929b40554233fa983fad9b
rube4489 <ruben.lopez@doc24.com.ar>
2022-08-11 10:44:38 -0300

    Agrego modelos

---
+++ src/models/PokemonList.ts
@@ -0,0 +1,42 @@
+export class PokemonsName {
+    pokemon1: string;
+    pokemon2: string;
+    pokemon3: string;
+    pokemon4: string;
+    pokemon5: string;
+    pokemon6: string;
+    pokemon7: string;
+    pokemon8: string;
+    pokemon9: string;
+    pokemon10: string;
+    pokemon11: string;
+    pokemon12: string;
+    constructor(
+      pokemon1: string,
+      pokemon2: string,
+      pokemon3: string,
+      pokemon4: string,
+      pokemon5: string,
+      pokemon6: string,
+      pokemon7: string,
+      pokemon8: string,
+      pokemon9: string,
+      pokemon10: string,
+      pokemon11: string,
+      pokemon12: string
+    ) {
+      this.pokemon1 = pokemon1;
+      this.pokemon2 = pokemon2;
+      this.pokemon3 = pokemon3;
+      this.pokemon4 = pokemon4;
+      this.pokemon5 = pokemon5;
+      this.pokemon6 = pokemon6;
+      this.pokemon7 = pokemon7;
+      this.pokemon8 = pokemon8;
+      this.pokemon9 = pokemon9;
+      this.pokemon10 = pokemon10;
+      this.pokemon11 = pokemon11;
+      this.pokemon12 = pokemon12;
+    }
+  }
+  
